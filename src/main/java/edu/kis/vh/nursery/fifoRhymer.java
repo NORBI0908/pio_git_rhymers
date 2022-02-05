94d3aee53bd4cfbea341b0e0c84b248d701cad45
Norbert Wilicki <229330@edu.p.lodz.pl>
2022-02-05 14:20:08 +0100

    usuwanie konfliktu do marge z master - zmienna total

--- src/main/java/edu/kis/vh/nursery/fifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/fifoRhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class fifoRhymer extends defaultCountingOutRhymer {
 
-	private defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+	public final defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
 
 	@Override
 	public int countOut() {
